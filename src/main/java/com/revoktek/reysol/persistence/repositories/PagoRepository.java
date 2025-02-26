d4da60dc2020fb9f418e9046a7440beaa5ee6e7d
KeniaReyes <kreyes@revoktek.com>
2025-02-25 18:03:43 -0600

    Obtener pago por id pago

--- src/main/java/com/revoktek/reysol/persistence/repositories/PagoRepository.java
+++ src/main/java/com/revoktek/reysol/persistence/repositories/PagoRepository.java
@@ -1,12 +1,13 @@
 package com.revoktek.reysol.persistence.repositories;
 
-import com.revoktek.reysol.persistence.entities.Pago;
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
-import java.util.List;
+import com.revoktek.reysol.persistence.entities.Pago;
 
 public interface PagoRepository extends JpaRepository<Pago, Long> {
 
@@ -22,5 +23,14 @@ public interface PagoRepository extends JpaRepository<Pago, Long> {
                 estatusPago.idEstatusPago = :idEstatusPago
             """)
     BigDecimal findAbonadoByPedido(@Param("idPedido") Long idPedido, @Param("idEstatusPago") Integer idEstatusPago);
+
+    @Query("""
+            SELECT p
+            FROM Pago p
+            INNER JOIN FETCH p.pedido pedido
+            WHERE pedido.idPedido = :idPedido
+            ORDER BY p.fechaRegistro DESC
+            """)
+            List<Pago> findByPedidoId(Long idPedido);
 
 }
